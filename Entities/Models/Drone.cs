aa2d5ba3a4c722aa4ba518d53957784041b5e182
AhmadShahramAreeb <shahram.1732@gmail.com>
2026-01-27 03:38:29 +0100

    Add Drone entity model,TBC.

---
+++ Entities/Models/Drone.cs
@@ -0,0 +1,16 @@
+namespace Entities.Models;
+
+public class Drone
+{
+    public int Id { get; set; }
+    public string Model { get; set; }
+
+    // Serial number for tracking.
+    // Takip için seri numarası.
+    public string SerialNumber { get; set; }
+    public double BatteryLevel { get; set; }
+
+    // Current operational status: "Idle", "Flying", "Charging", "Maintenance".
+    // Mevcut operasyonel durum: "Boşta", "Uçuşta", "Şarjda", "Bakımda".
+    public string Status { get; set; }
+}
