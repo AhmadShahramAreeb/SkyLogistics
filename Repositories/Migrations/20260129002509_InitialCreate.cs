560584863f9ec2f82f8c9b0ef0ecd8edb5f7f397
AhmadShahramAreeb <shahram.1732@gmail.com>
2026-01-29 02:02:05 +0100

    Add InitialCreate migration for Drones table
    - Create `InitialCreate` migration to establish the `Drones` table schema.
    - Define columns for `Id`, `Model`, `SerialNumber`, `BatteryLevel`, and `Status`.
    - Add `RepositoryContextModelSnapshot` to snapshot the current model configuration.

---
+++ Repositories/Migrations/20260129002509_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SerialNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BatteryLevel = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drones", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drones");
+        }
+    }
+}
